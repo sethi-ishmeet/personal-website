34d0e30ee3185d00ac53815d97231cafd4851958
Ishmeet Sethi <sethi.ishmeet@gmail.com>
2020-08-06 04:41:16 +0530

    published basic site

---
+++ Sources/PersonalWebsite/CustomTheme.swift
@@ -0,0 +1,151 @@
+import Plot
+import Publish
+
+extension Theme where Site == PersonalWebsite {
+    static var custom: Self {
+        Theme(
+            htmlFactory: CustomHTMLFactory(),
+            resourcePaths: [
+                "Resources/CustomTheme/styles.css"
+            ]
+        )
+    }
+    
+    private struct CustomHTMLFactory: HTMLFactory {
+        func makeIndexHTML(for index: Index, context: PublishingContext<PersonalWebsite>) throws -> HTML {
+            HTML(
+                .head(for: index, on: context.site),
+                .body(
+                    .header(for: context, selectedSection: nil),
+                    .p(
+                        .class("description"),
+                        .text(context.site.description),
+                        .a(
+                            .text("Simplified Automation"),
+                            .href("https://simplifiedautomation.ca"),
+                            .target(.blank)
+                        )
+                    ),
+                    .img(
+                        .src(context.site.imagePath!),
+                        .class("image-cropper")
+                    ),
+                    .wrapper(
+                        .a(
+                            .img(
+                                .src(context.site.twitter),
+                                .class("social")
+                            ),
+                            .href("https://twitter.com/IshmeetSing"),
+                            .target(.blank)
+                        ),
+                        .a(
+                            .img(
+                                .src(context.site.linkedin),
+                                .class("social")
+                            ),
+                            .href("https://www.linkedin.com/in/ishmeetsinghsethi"),
+                            .target(.blank)
+                        ),
+                        .a(
+                            .img(
+                                .src(context.site.github),
+                                .class("social")
+                            ),
+                            .href("https://github.com/sethi-ishmeet"),
+                            .target(.blank)
+                        )
+                    ),
+                    .span(),
+                    .footer(for: context.site)
+                )
+            )
+        }
+        
+        func makeSectionHTML(for section: Section<PersonalWebsite>, context: PublishingContext<PersonalWebsite>) throws -> HTML {
+            HTML()
+        }
+        
+        func makeItemHTML(for item: Item<PersonalWebsite>, context: PublishingContext<PersonalWebsite>) throws -> HTML {
+            HTML()
+        }
+        
+        func makePageHTML(for page: Page, context: PublishingContext<PersonalWebsite>) throws -> HTML {
+            HTML(
+                .lang(context.site.language),
+                .head(for: page, on: context.site),
+                .body(
+                    .header(for: context, selectedSection: nil),
+                    .wrapper(.contentBody(page.body)),
+                    .footer(for: context.site)
+                )
+            )
+        }
+        
+        func makeTagListHTML(for page: TagListPage, context: PublishingContext<PersonalWebsite>) throws -> HTML? {
+            return nil
+        }
+        
+        func makeTagDetailsHTML(for page: TagDetailsPage, context: PublishingContext<PersonalWebsite>) throws -> HTML? {
+            return nil
+        }
+        
+        
+        
+    }
+}
+
+
+private extension Node where Context == HTML.BodyContext {
+    static func wrapper(_ nodes: Node...) -> Node {
+        .div(.class("wrapper"), .group(nodes))
+    }
+    
+    static func header<T: Website>(
+        for context: PublishingContext<T>,
+        selectedSection: T.SectionID?
+    ) -> Node {
+        return .header(
+            .wrapper(
+                .a(.class("site-name"), .href("/"), .text(context.site.name))
+            )
+        )
+    }
+    
+    static func itemList<T: Website>(for items: [Item<T>], on site: T) -> Node {
+        return .ul(
+            .class("item-list"),
+            .forEach(items) { item in
+                .li(.article(
+                    .h1(.a(
+                        .href(item.path),
+                        .text(item.title)
+                        )),
+                    .tagList(for: item, on: site),
+                    .p(.text(item.description))
+                    ))
+            }
+        )
+    }
+    
+    static func tagList<T: Website>(for item: Item<T>, on site: T) -> Node {
+        return .ul(.class("tag-list"), .forEach(item.tags) { tag in
+            .li(.a(
+                .href(site.path(for: tag)),
+                .text(tag.string)
+                ))
+            })
+    }
+    
+    static func footer<T: Website>(for site: T) -> Node {
+        return .footer(
+            .p(
+                .text("Generated using "),
+                .a(
+                    .text("Publish"),
+                    .href("https://github.com/johnsundell/publish")
+                )
+            )
+        )
+    }
+}
--- Sources/PersonalWebsite/main.swift
+++ Sources/PersonalWebsite/main.swift
@@ -6,20 +6,23 @@ import Plot
 struct PersonalWebsite: Website {
     enum SectionID: String, WebsiteSectionID {
         // Add the sections that you want your website to contain here:
-        case posts
+        case introduction
     }
 
     struct ItemMetadata: WebsiteItemMetadata {
         // Add any site-specific metadata that you want to use here.
     }
-
+    
     // Update these properties to configure your website:
-    var url = URL(string: "https://virtualcoder.io")!
+    var url = URL(string: "https://ishmeet.me")!
     var name = "Ishmeet Sethi"
-    var description = "iOS Developer. CTO @ Simplified Automation."
+    var description = "iOS Developer. CTO @ "
     var language: Language { .english }
-    var imagePath: Path? { nil }
+    var imagePath: Path? { "Images/profile.jpg" }
+    var github: Path { "Images/github.png" }
+    var twitter: Path { "Images/twitter.png" }
+    var linkedin: Path { "Images/linkedin.png" }
 }
 
 // This will generate your website using the built-in Foundation theme:
-try PersonalWebsite().publish(withTheme: .foundation)
+try PersonalWebsite().publish(withTheme: .custom)
